fe68443e710715ab1cca32558f4a94368d7c9f32
Dhiyanu290100 <Dhiyanu290100@gmail.com>
2022-11-18 15:09:00 +0530

    group list container

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,21 +3,20 @@ import { StreamChat } from 'stream-chat';
 import { Chat } from 'stream-chat-react';
 import Cookies from "universal-cookie";
 
+
+import './Styles/App.css';
 import { GroupContainer, GroupListContainer } from "./Components";
 
 const apikey = 'tr7y6szxcqbd';
 const client = StreamChat.getInstance(apikey);
+
 const App = () => {
   return (
-    <div className='app_wrapper'>
-      <h1>Chat Application</h1>
-      <Chat client={client} theme="theme dark">
-        <GroupListContainer 
-        
-        />
-        <GroupContainer 
-        
-        />
+    <div className="app__wrapper">
+      {/* <h1>Chat Application</h1> */}
+      <Chat client={client} theme="team light">
+        <GroupListContainer />
+        <GroupContainer />
       </Chat>
     </div>
   )
--- client/src/Components/GroupListContainer.jsx
+++ client/src/Components/GroupListContainer.jsx
@@ -1,10 +1,35 @@
-import React from 'react'
+import React from 'react';
+import { ChannelList, useChatContext } from "stream-chat-react";
+import Cookies from "universal-cookie";
 
+import icon1  from "../assets/icon1.png";
+import LogoutIcon from "../assets/logout.png";
+import '../Styles/App.css';
+
+
+import { ChannelSearch, TeamChannelList, TeamChannelPreview } from "./";
+
+
+const SideBar = () => (
+  <div classname='group-list__sidebar'>                                                                                                                                        
+      <div classname="group-list__sidebar__icon1">
+        <div classname="icon1__inner">
+          <img src={icon1} alt = "icon1" width="30"/>
+        </div>
+      </div>
+      <div classname="group-list__sidebar__icon2">   
+        <div classname="icon2__inner">
+          <img src={LogoutIcon} alt = "Logout" width="30"/>
+        </div>
+      </div>
+  </div>
+  
+)
 const GroupListContainer = () => {
   return (
-    <div>
-      GroupListContainer
-    </div>
+    <>
+      <SideBar />
+    </>
   )
 }
 
